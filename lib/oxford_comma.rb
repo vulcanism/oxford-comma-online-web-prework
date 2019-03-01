ecef2929d1808cfe986646668219416b5947b3ed
Savannah C <sgcorrell@gmail.com>
2019-03-01 17:27:05 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
+  
+  if array.size == 1 
+    array.join
+  elsif array.size == 2 
+    array.join(" and ")
+  else
+    array[0...-1].join(", ") << ", and #{array[-1]}"
+  end
 
 end
